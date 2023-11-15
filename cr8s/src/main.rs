233f2c0f786646964d4ba0b3ed3b8806d9ce1eb0
Evgeny <evgeny.hyvarinen@tietoevry.com>
2023-11-15 16:31:06 +0200

    Added routes in main

--- cr8s/src/main.rs
+++ cr8s/src/main.rs
@@ -5,5 +5,17 @@ mod schema;
 
 #[rocket::main]
 async fn main() {
-    let _ = rocket::build().mount("/", rocket::routes![]).launch().await;
+    let _ = rocket::build()
+        .mount(
+            "/",
+            rocket::routes![
+                rocket_routes::rustaceans::get_rustaceans,
+                rocket_routes::rustaceans::view_rustacean,
+                rocket_routes::rustaceans::create_rustacean,
+                rocket_routes::rustaceans::update_rustacean,
+                rocket_routes::rustaceans::delete_rustacean,
+            ],
+        )
+        .launch()
+        .await;
 }
